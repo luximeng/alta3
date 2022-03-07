9df58d90fb654cd82e6459fd65007fb628939901
luximeng <lucymeng2009@gmail.com>
2022-03-07 17:51:05 +0000

    Learning to work with the matplotlib library

---
+++ cust_if_script.py
@@ -0,0 +1,23 @@
+import random
+
+def card():
+    color = ['heart', 'diamonds', 'spades', 'clubs']
+    value = ['Ace','2','3','4','5','6','7','8','9','10','Jake','Queen','King']
+    print(random.choice(color), random.choice(value))
+
+card1 = card()
+card2 = card()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
